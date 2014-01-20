841592533d10bf974f87b6cc4f47073e11182b09
Alexander Lomov <xollvo@gmail.com>
2014-01-20 15:07:36 +0400

    New version with PDF#renderContent to render pdf from file content

--- fopdf/src/play/modules/fopdf/PDF.java
+++ fopdf/src/play/modules/fopdf/PDF.java
@@ -6,10 +6,23 @@ import java.util.Map;
 
 import play.classloading.enhancers.LocalvariablesNamesEnhancer.LocalVariablesNamesTracer;
 import play.classloading.enhancers.LocalvariablesNamesEnhancer.LocalVariablesSupport;
+import play.templates.BaseTemplate;
 import play.templates.Template;
 import play.templates.TemplateLoader;
 
 public class PDF implements LocalVariablesSupport {
+	
+	private static Map<String, Object> templateBindings(Object... args) {
+		Map<String, Object> templateBinding = new HashMap<String, Object>(16);
+        for (Object o : args) {
+            List<String> names = LocalVariablesNamesTracer.getAllLocalVariableNames(o);
+            for (String name : names) {
+                templateBinding.put(name, o);
+            }
+        }
+        
+        return templateBinding;
+	}
 
 	/**
 	 * Render template with data from args
@@ -19,16 +32,18 @@ public class PDF implements LocalVariablesSupport {
 	 */
 	public static void render(String fop_template_path, Object... args) {
 		Template template = TemplateLoader.load(fop_template_path);
-		
-		Map<String, Object> templateBinding = new HashMap<String, Object>(16);
-        for (Object o : args) {
-            List<String> names = LocalVariablesNamesTracer.getAllLocalVariableNames(o);
-            for (String name : names) {
-                templateBinding.put(name, o);
-            }
-        }
-        
-        throw new RenderPdfTemplate(template, templateBinding);
+        throw new RenderPdfTemplate(template, templateBindings(args));
+	}
+	
+	/**
+	 * Render template from its content (as string)
+	 * 
+	 * @param template_content
+	 * @param args
+	 */
+	public static void renderContent(String template_content, Object... args) {
+		Template template = TemplateLoader.loadString(template_content);
+		throw new RenderPdfTemplate(template, templateBindings(args));
 	}
 
 }
--- samples-and-tests/pdf-sample-app/app/controllers/Application.java
+++ samples-and-tests/pdf-sample-app/app/controllers/Application.java
@@ -1,12 +1,28 @@
 package controllers;
 
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import play.modules.fopdf.PDF;
 import play.mvc.Controller;
+import play.vfs.VirtualFile;
 
 public class Application extends Controller {
 
     public static void index() {
-        PDF.render("pdf/test.fo");
+    	String text = "asd asd asd asd и русские буквы";
+        PDF.render("pdf/test.fo", text);
+    }
+    
+    public static void index2() throws IOException {
+    	VirtualFile template_vf = VirtualFile.fromRelativePath("app/views/pdf/test.fo");
+    	if(template_vf == null)
+    		throw new IOException("No such file");
+    	
+    	String text = "фавпук ывфы ваыва вып варп вап ываываыва ыва";
+		String template_content = FileUtils.readFileToString(template_vf.getRealFile());
+		PDF.renderContent(template_content, text);
     }
 
 }
